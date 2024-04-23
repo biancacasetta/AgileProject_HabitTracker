bd6c6ba183bd56d329a7bd87634810baef4bc693
Oscar Nilsson <oscarhedbergn@gmail.com>
2024-04-23 11:28:08 +0200

    Added attributes with getters and setters to the Habit class and added creating, deleting and editing habits in the HabitService class.

--- src/app/Habit.java
+++ src/app/Habit.java
@@ -1,4 +1,31 @@
 package app;
 
 public class Habit {
+    private String name;
+    private String desc;
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+
+    public Habit(String name, String desc) {
+        this.name = name;
+        this.desc = desc;
+    }
+
+
+
 }
+
---
+++ src/app/HabitService.java
@@ -0,0 +1,36 @@
+package app;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Objects;
+
+public class HabitService {
+    ArrayList<Habit> listOfHabits = new ArrayList<Habit>();
+
+    public void createHabit(String n, String d) {
+        Habit habit = new Habit(n, d);
+        listOfHabits.add(habit);
+    }
+
+    public void deleteHabit(String hn) {
+        listOfHabits.removeIf(habit -> Objects.equals(hn, habit.getName()));
+    }
+
+    public Habit getHabitFromList(String name) {
+        for (Habit habit : listOfHabits) {
+            if (Objects.equals(habit.getName(), name)) {
+                return habit;
+            }
+        }
+        return null;
+    }
+
+    public Habit editHabit(String name) {
+        Habit habit = getHabitFromList(name);
+        String placeholder = "placeholder";
+        habit.setName(placeholder);
+        habit.setDesc(placeholder);
+        return habit;
+    }
+
+}
