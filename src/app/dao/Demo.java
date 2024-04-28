0b2d3bee0b96b687d39b47cd471ebdfdb0628b90
Antoine <antoine.geiger0038@stud.hkr.se>
2024-04-28 17:22:00 +0200

    added Demo (test file)

---
+++ src/app/dao/Demo.java
@@ -0,0 +1,29 @@
+package app.dao;
+
+import app.DBConnection;
+
+import java.sql.Connection;
+
+import java.io.IOException;
+import java.sql.*;
+
+public class Demo {
+    public static void main(String[] args) throws IOException {
+        try (Connection connection = DBConnection.getConnection();
+             ResultSet resultSet = connection
+                     .createStatement()
+                     .executeQuery("SELECT * FROM activeHabits")) {
+
+            while (resultSet.next()) {
+                int customerId = resultSet.getInt("id");
+                String lastName = resultSet.getString("name");
+                String firstName = resultSet.getString("desc");
+                System.out.printf("ID: %d, Name: %s, description: %s%n",
+                        customerId, lastName, firstName);
+            }
+        } catch (SQLException e) {
+            System.err.println("Error: " + e.getMessage());
+            e.printStackTrace();
+        }
+    }
+}
